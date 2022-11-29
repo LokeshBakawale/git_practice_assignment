0b11d4d3b0bad10581d2de49cd29d2f134b6afad
unknown <m24killerfrost221@gmail.com>
2022-11-29 22:29:09 +0530

    Prime Number Done

--- prime_number.js
+++ prime_number.js
@@ -1,9 +1,17 @@
-let number=5;
-
-for(let i=1; i<=number; i++){
-
-if(number%2==0){
-console.log(A Prime Number){
-
-}else{
-console.log(Not A Prime Number)
+let num = 11;
+let count = 0;
+let bag = 0;
+for (let i = 1; i <= num; i++){
+  if(num % i == 0){
+    count++;
+  }
+  else{
+    bag++;
+  }
+}
+if (count == 2){
+  console.log(num + ' ' +'is Prime');
+}
+else{
+  console.log(num + ' ' +'is not Prime');
+}
